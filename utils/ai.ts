e64f171d0c0285f2789539b51b11148fef502e31
ddoyediran <ddoyediran@gmail.com>
2023-06-15 22:41:48 +0200

    Setting up the ai part, aslo handling the structure of the prompt, adding schemaType to it

---
+++ utils/ai.ts
@@ -0,0 +1,53 @@
+import { OpenAI } from "langchain/llms/openai";
+import { StructuredOutputParser } from "langchain/output_parsers";
+import { PromptTemplate } from "langchain/prompts";
+
+import z from "zod";
+
+// create a schemaType and Parse the output from the model
+const parser = StructuredOutputParser.fromZodSchema(
+  z.object({
+    mood: z
+      .string()
+      .describe("the mood of the person who wrote the journal entry."),
+    subject: z.string().describe(""),
+    summary: z.string().describe("quick summary of the entire entry."),
+    negative: z
+      .boolean()
+      .describe(
+        "is the journal entry negative? (i.e. does it contain negative emotions?)."
+      ),
+    color: z
+      .string()
+      .describe(
+        "a hexidecimal color code that represents the mood of the entry. Example #0101fe for blue representing happiness."
+      ),
+  })
+);
+
+const getPrompt = async (content) => {
+  const format_instructions = parser.getFormatInstructions();
+
+  const prompt = new PromptTemplate({
+    template:
+      "Analyze the following journal entry. Follow the instructions and format your response to match the format instructions, no matter what! \n{format_instructions}\n{entry}",
+    inputVariables: ["entry"],
+    partialVariables: { format_instructions },
+  });
+
+  const input = await prompt.format({
+    entry: content,
+  });
+
+  console.log(input);
+  return input;
+};
+
+export const analyze = async (content) => {
+  const input = await getPrompt(content);
+  const model = new OpenAI({ temperature: 0, model: "gpt-3.5-turbo" });
+
+  const result = await model.call(input);
+
+  console.log(result);
+};
